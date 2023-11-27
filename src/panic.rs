d22030e1181540e62817761ff099ff44f5f2142a
Krzysztof Mazur <krzmazur1@gmail.com>
2023-11-27 17:25:50 +0100

    Fix panic handler by disabling interrupts

--- src/panic.rs
+++ src/panic.rs
@@ -3,8 +3,8 @@ use core::panic::PanicInfo;
 #[inline(never)]
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
-    // TODO: It seems like this only runs after the executor runs another task
-    // it's something, but immediate reset on panic would be better ¯\_(ツ)_/¯
+    // Important to disable the interrupts, otherwise the execution might halt before resetting
+    cortex_m::interrupt::disable();
     unsafe {
         let p = embassy_rp::Peripherals::steal();
         let mut w = embassy_rp::watchdog::Watchdog::new(p.WATCHDOG);
